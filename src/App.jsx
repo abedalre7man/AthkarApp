5fda9d2c6eb6f566c8df529bab5e54cfc2ed6d07
Abed Alrehman Nasser Mohammed <221181@ppu.edu.ps>
2025-07-10 21:15:22 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import './App.css';
+import AzkarCards from "./components/AzkarCards";
+
+function App() {
+  return (
+    <div>
+      <h1 className="page-title">أذكار المسلم</h1>
+      <AzkarCards />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AzkarCards.jsx
@@ -0,0 +1,66 @@
+// src/AzkarCards.jsx
+import React, { useEffect, useState } from 'react';
+import './AzkarCards.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
+import { cardsData } from "../cardsData.js";
+
+function AzkarCards() {
+  const [cards, setCards] = useState([]);
+
+useEffect(() => {
+  const updated = cardsData.map(card => ({
+    ...card,
+    currentCount: card.count 
+  }));
+  setCards(updated);
+}, []);
+
+const handleCardClick = (id) => {
+  setCards(prevCards =>
+    prevCards.map(card =>
+      card.id === id && card.currentCount > 0
+        ? { ...card, currentCount: card.currentCount - 1 }
+        : card
+    )
+  );
+};
+
+
+const handleReset = (id) => {
+  setCards(prevCards =>
+    prevCards.map(card =>
+      card.id === id ? { ...card, currentCount: card.count } : card
+    )
+  );
+};
+
+
+  return (
+    <div className="cards-container">
+      {cards.map(card => (
+        <div
+          className="card"
+          key={card.id}
+          style={{ backgroundColor: card.backgroundColor }}
+          onClick={() => handleCardClick(card.id)}
+        >
+          <h3 className="card-title">{card.title}</h3>
+          <button
+            className="refresh-btn"
+            onClick={(e) => {
+              e.stopPropagation();
+              handleReset(card.id);
+            }}
+          >
+            <FontAwesomeIcon icon={faArrowsRotate} />
+          </button>
+          <h1>{card.currentCount}</h1>
+<p className="card-total">{card.count}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AzkarCards;
